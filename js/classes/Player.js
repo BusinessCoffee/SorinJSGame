cbc28229b462a9c102e2302846ff05e898286518
Levi Meltabarger <levi.businesscoffee@gmail.com>
2023-08-20 23:55:56 -0700

    collision fix with x axis. But new bug with y axis

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -43,13 +43,34 @@ class Player {
                     }
 
                     if (this.velocity.x > 1) {
-                        this.position.x = collisionBlock.position.x = this.width -0.01
+                        this.position.x = collisionBlock.position.x - this.width - 0.01
                         break
                     }
                 }
         }
         this.position.y += this.velocity.y
         this.sides.bottom = this.position.y + this.height
+
+        for (let i = 0; i < this.collisionBlocks.length; i++) {
+            const collisionBlock = this.collisionBlocks[i]
+            if (this.position.x <= collisionBlock.position.x + collisionBlock.width &&
+                this.position.x + this.width >= collisionBlock.position.x && 
+                this.position.y + this.height >= collisionBlock.position.y && 
+                this.position.y <= collisionBlock.position.y + collisionBlock.height) 
+                {   
+                    // collision on x axis going to the left
+                    if (this.velocity.y < -1) {
+                        this.position.y = collisionBlock.position.y + collisionBlock.height + 0.01
+                        break
+                    }
+
+                    if (this.velocity.y > 1) {
+                        this.position.y = collisionBlock.position.y - this.height - 0.01
+                        break
+                    }
+                }
+        }
+
         
         // above bottom of canvas
         if (this.sides.bottom + this.velocity.y < canvas.height) {
