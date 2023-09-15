0748d120d14aa7d998bbfc937a7197fc0c84c6e7
James <jhdonahue41@gmail.com>
2023-09-14 17:10:35 -0700

    LET'S GOOOOOOOOOOO it works and stuff buttons clic

--- js/classes/ShopUI.js
+++ js/classes/ShopUI.js
@@ -22,53 +22,58 @@ class shop {
         pistol.src = './img/icon/pistol.png'
         const rifle = new Image()
         rifle.src = './img/icon/rifle.png'
-        
+        //+550
             
         rifle.onload = () => {
-            console.log("howdy sir")
-            c.drawImage(baton, 700, 700, 50,50)
-            c.drawImage(knife, 50, 150, 50,50)
-            c.drawImage(sword, 50, 250, 50,50)
-            c.drawImage(photon, 50, 350, 50,50)
-            c.drawImage(crossbow, 150, 50, 50,50)
-            c.drawImage(revolver, 150, 150, 50,50)
-            c.drawImage(pistol, 150, 250, 50,50)
-            c.drawImage(rifle, 150, 350, 50,50)
+            c.drawImage(baton,  50,50+550, 50,50)
+            c.drawImage(knife,  150,50+550, 50,50)
+            c.drawImage(sword,  250,50+550, 50,50)
+            c.drawImage(photon,  350,50+550, 50,50)
+            c.drawImage(crossbow,  50,150+550, 50,50)
+            c.drawImage(revolver,  150,150+550, 50,50)
+            c.drawImage(pistol,  250,150+550, 50,50)
+            c.drawImage(rifle,  350,150+550, 50,50)
         }
 
         
         
+        
+    }
+
+    button() {
         canvas.addEventListener("click", function (event) {
-            console.log("clicked")
             const hbox = canvas.getBoundingClientRect()
             const x = event.clientX - hbox.left
             const y = event.clientY - hbox.top
-                if (x >= 50 && x <= 100 && y >= 50 && y <= 100) {
+                if (x >= 50 && x <= 100 && y >= 50+550 && y <= 100+550) {
                     alert('give baton')
                 }
-                if (x >= 150 && x <= 200 && y >= 50 && y <= 100) {
+                if (x >= 150 && x <= 200 && y >= 50+550 && y <= 100+550) {
                     alert('give knife')
                 }
-                if (x >= 250 && x <= 300 && y >= 50 && y <= 100) {
+                if (x >= 250 && x <= 300 && y >= 50+550 && y <= 100+550) {
                     alert('give sword')
                 }
-                if (x >= 350 && x <= 400 && y >= 50 && y <= 100) {
+                if (x >= 350 && x <= 400 && y >= 50+550 && y <= 100+550) {
                     alert('give photon')
                 }
-                if (x >= 50 && x <= 100 && y >= 150 && y <= 200) {
+                if (x >= 50 && x <= 100 && y >= 150+550 && y <= 200+550) {
                     alert('give crossbow')
                 }
-                if (x >= 150 && x <= 200 && y >= 150 && y <= 200) {
+                if (x >= 150 && x <= 200 && y >= 150+550 && y <= 200+550) {
                     alert('give revolver')
                 }
-                if (x >= 250 && x <= 300 && y >= 150 && y <= 200) {
+                if (x >= 250 && x <= 300 && y >= 150+550 && y <= 200+550) {
                     alert('give pistol')
                 }
-                if (x >= 350 && x <= 400 && y >= 150 && y <= 200) {
+                if (x >= 350 && x <= 400 && y >= 150+550 && y <= 200+550) {
                     alert('give rifle')
                 }
+                
+            
         })
-        
-        
     }
 }
+
+        
+        
